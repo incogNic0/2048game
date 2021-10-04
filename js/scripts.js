a291ec67362043b486b471301b6aa618fc665aa9
incogNic0 <incognico.dev@gmail.com>
2021-10-03 21:03:41 -0500

    add outer container to content and event listeners for buttons

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+const controlBtns = document.querySelector('.controls-container');
+const scoreboardBtns = document.querySelector('.scoreboard-buttons');
+
+// === EVENT LISTENERS ===
+
+// KEYPRESS
+window.addEventListener('keydown', (e) => {
+  switch(e.code) {
+    case 'ArrowUp':
+      moveUp();
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+    case 'ArrowRight':
+      moveRight();
+    default:
+      break;
+  }
+});
+
+// DIRECTIONAL ARROWS
+controlBtns.addEventListener('click', e => {
+  console.log(e.target.id);
+})
+
+// NEW GAME / UNDO
+scoreboardBtns.addEventListener('click', e => {
+  if(e.target.id === 'new-game' || e.target.parentElement.id === 'new-game') {
+    console.log('new game!');
+  } else if (e.target.id = 'undo' || e.target.parentElement.id === 'undo'  ) {
+    console.log('undo!');
+  }
+});
+
+function moveRight() {
+  console.log('Move right!');
+};
+
+function moveLeft() {
+  console.log('Move left!');
+};
+
+function moveUp() {
+  console.log('Move up!');
+};
+
+function moveDown() {
+  console.log('Move down!');
+};

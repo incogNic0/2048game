e60987840ee6b13599679d63bc74a23d5ca87f8b
incogNic0 <incognico.dev@gmail.com>
2021-10-04 13:29:22 -0500

    refactor random tile function. add gameboard state tracking

--- js/game.js
+++ js/game.js
@@ -1,6 +1,21 @@
-function newRandomTile() {
+const currentBoard = {
+  row0: [0,0,0,0],
+  row1: [0,0,0,0],
+  row2: [0,0,0,0],
+  row3: [0,0,0,0]
+}
+
+let previousBoard;
+
+function generateRandomTile() {
 	const value = getValue(); // Generate random value (2 or 4) for new box
-	displayNewTile(value);
+	const box = selectRandomBox(); // Get random box for new tile location
+  if(box) {
+    occupyBox(box,value); // Set tile value, display, and mark as occupied
+    updateBoardArrays(box, value);
+  } else {
+    gameOver();
+  }
 }
 
 function getValue() {
@@ -8,23 +23,27 @@ function getValue() {
 	return nums[Math.round(Math.random() * 1)];
 }
 
-function displayNewTile(value) {
+function selectRandomBox() {
   // Get all unoccupied boxes
-	const unoccupiedBoxes = document.querySelectorAll('[data-occupied="false"]');
+  const unoccupiedBoxes = document.querySelectorAll('[data-occupied="false"]');
   const numBoxes = unoccupiedBoxes.length;
-  if (numBoxes) {
-    // Select a random unoccupied box for new tile location
-    const box = unoccupiedBoxes[Math.floor(Math.random() * numBoxes)];
-    occupyBox(box,value); // Set tile value, display, and mark as occupied
-  } else {
-    return gameOver();
+  if (!numBoxes) {
+    return;
   }
+  // Return a random unoccupied box for new tile location
+  return unoccupiedBoxes[Math.floor(Math.random() * numBoxes)];
 }
 
 function occupyBox(boxElement, boxValue) {
   boxElement.children[0].classList.add('occupied');
   boxElement.setAttribute("data-occupied", "true")
   boxElement.querySelector('.value').textContent = boxValue;
+}
+
+function updateBoardArrays (boxElement, boxValue) {
+  const coords = boxElement.getAttribute("data-grid").split('-');
+  previousBoard = JSON.parse(JSON.stringify(currentBoard));
+  currentBoard[coords[0]][coords[1]] = boxValue;
 }
 
 function gameOver() {

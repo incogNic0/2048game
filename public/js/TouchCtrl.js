59cc92c4ee8f9a4de16aa37a061077cb1b6c901b
incogNic0 <incognico.dev@gmail.com>
2021-10-15 17:25:13 -0500

    move js/css folders in public folder. add GameCtl/TouchCtrl modules

---
+++ public/js/TouchCtrl.js
@@ -0,0 +1,82 @@
+const TouchCtrl = (function () {
+	// Handle the start of gestures
+	function handleSwipeStart(evt) {
+		evt.preventDefault();
+		if (evt.touches && evt.touches.length > 1) {
+			return;
+		}
+		if (window.PointerEvent) {
+			evt.target.setPointerCapture(evt.pointerId);
+		} else {
+			// Add Mouse Listeners
+			document.addEventListener("mousemove", handleSwipeMove, true);
+			document.addEventListener("mouseup", handleSwipeEnd, true);
+		}
+		initialTouchPos = getGesturePointFromEvent(evt);
+	}
+
+	// Handle end gestures
+	function handleSwipeEnd(evt) {
+		evt.preventDefault();
+		if (evt.touches && evt.touches.length > 0) {
+			return;
+		}
+		// Remove Event Listeners
+		if (window.PointerEvent) {
+			evt.target.releasePointerCapture(evt.pointerId);
+		} else {
+			// Remove Mouse Listeners
+			document.removeEventListener("mousemove", handleSwipeMove, true);
+			document.removeEventListener("mouseup", handleSwipeEnd, true);
+		}
+		moveTilesSwipe();
+		initialTouchPos = null;
+	}
+
+	function getGesturePointFromEvent(evt) {
+		const point = {};
+		if (evt.targetTouches) {
+			// Prefer Touch Events
+			point.x = evt.targetTouches[0].clientX;
+			point.y = evt.targetTouches[0].clientY;
+		} else {
+			// Either Mouse event or Pointer Event
+			point.x = evt.clientX;
+			point.y = evt.clientY;
+		}
+		return point;
+	}
+
+	function handleSwipeMove(evt) {
+		evt.preventDefault();
+		if (!initialTouchPos) {
+			return;
+		}
+		lastTouchPos = getGesturePointFromEvent(evt);
+	}
+
+	function moveTilesSwipe() {
+		const horitonalSwipe = lastTouchPos.x - initialTouchPos.x;
+		const verticalSwipe = lastTouchPos.y - initialTouchPos.y;
+		if (Math.abs(horitonalSwipe) > Math.abs(verticalSwipe)) {
+			if (horitonalSwipe > 0) {
+				GameCtrl.moveTiles("right");
+			} else {
+				GameCtrl.moveTiles("left");
+			}
+		} else {
+			if (verticalSwipe < 0) {
+				GameCtrl.moveTiles("up");
+			} else {
+				GameCtrl.moveTiles("down");
+			}
+		}
+	}
+  return {
+    handleSwipeStart, 
+    handleSwipeEnd, 
+    getGesturePointFromEvent, 
+    handleSwipeMove,
+    moveTilesSwipe
+  }
+})();
---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+const controlBtns = document.querySelector('.controls-container');
+const scoreboardBtns = document.querySelector('.scoreboard-buttons');
+const gameBoard = document.querySelector('.game-board');
+let initialTouchPos;
+let lastTouchPos;
+
+// === EVENT LISTENERS ===
+window.onload = function() {
+  GameCtrl.newGame();
+}
+
+// ====== Swipe Support ===============
+// Check if pointer events are supported.
+if (window.PointerEvent) {
+  gameBoard.addEventListener('pointerdown', TouchCtrl.handleSwipeStart, true);
+  gameBoard.addEventListener('pointermove', TouchCtrl.handleSwipeMove, true);
+  gameBoard.addEventListener('pointerup', TouchCtrl.handleSwipeEnd, true);
+  gameBoard.addEventListener('pointercancel', TouchCtrl.handleSwipeEnd, true);
+} else {
+  // Add Touch Listener
+  gameBoard.addEventListener('touchstart', TouchCtrl.handleSwipeStart, true);
+  gameBoard.addEventListener('touchmove', TouchCtrl.handleSwipeMove, true);
+  gameBoard.addEventListener('touchend', TouchCtrl.handleSwipeEnd, true);
+  gameBoard.addEventListener('touchcancel', TouchCtrl.handleSwipeEnd, true);
+  
+    // Add Mouse Listener
+  gameBoard.addEventListener('mousedown', TouchCtrl.handleSwipeStart, true);
+}
+
+
+// KEYPRESS
+window.addEventListener('keydown', (e) => {
+  const validKeyInputs = {
+    'ArrowUp': 'up',
+    'ArrowDown': 'down',
+    'ArrowLeft': 'left',
+    'ArrowRight': 'right',
+  }
+  if(validKeyInputs[e.code]) {
+    GameCtrl.moveTiles(validKeyInputs[e.code]);
+  }
+})
+
+// NEW GAME / UNDO
+scoreboardBtns.addEventListener('click', e => {
+  if(e.target.id === 'new-game' || e.target.parentElement.id === 'new-game') {
+    GameCtrl.newGame();
+  } else if (e.target.id = 'undo' || e.target.parentElement.id === 'undo'  ) {
+    GameCtrl.undoMove();
+  }
+});
